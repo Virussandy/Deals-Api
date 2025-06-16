9b2c035fcea6ee851cb80d886e52911c772fd52c
sandeep <sandeepkumarsingh17797@gmail.com>
2025-06-17 01:39:51 +0530

    Utils code updated

--- browser.js
+++ browser.js
@@ -1,26 +1,29 @@
 // browser.js
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+import { randomUUID } from 'crypto';
 
 puppeteer.use(StealthPlugin());
 
 export const getBrowser = async () => {
+  const userDataDir = `/tmp/chrome-user-data-${randomUUID()}`;
+
   const browser = await puppeteer.launch({
     headless: 'new',
     args: [
-        '--disable-gpu',
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--disable-features=site-per-process',
-        '--disable-breakpad',
-        '--no-zygote',
-        '--disable-web-security',
-        '--start-maximized',
-        '--window-size=1366,768',
-        '--user-data-dir=/tmp/chrome-user-data',
-      ],
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-accelerated-2d-canvas',
+      '--disable-features=site-per-process',
+      '--disable-breakpad',
+      '--no-zygote',
+      '--disable-web-security',
+      '--start-maximized',
+      '--window-size=1366,768',
+      `--user-data-dir=${userDataDir}`,
+    ],
   });
 
   const pages = await browser.pages();
